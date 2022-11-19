3b44e6bc3a882ab78ccebdd54f06e884232da067
mansivakte <mansi25vv@gmail.com>
2022-11-19 13:01:39 +0530

    --

---
+++ src/components/UI/Model.js
@@ -0,0 +1,10 @@
+import classes from "./Model.modeule.css";
+
+const Backdrop = () => {
+  return <div className={classes.backdrop}></div>;
+};
+const ModelOverlay = (props) => {
+  return;
+};
+
+export default Model;
